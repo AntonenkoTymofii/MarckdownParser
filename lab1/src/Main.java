2d93c0629543c456665b0afb1d6977b511a37e8b
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-03-02 01:36:35 +0200

    create new module

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,15 +1,8 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 public class Main {
-    private static final Pattern boldRegEx = Pattern.compile("\\*\\*(.*?)\\*\\*", Pattern.DOTALL);
-    private static final Pattern italicRegEx = Pattern.compile("_(.*?)_", Pattern.DOTALL);
-    private static final Pattern monoRegEx = Pattern.compile("`(.*?)`", Pattern.DOTALL);
-    private static final Pattern preFormRegEx = Pattern.compile("```(.*?)```", Pattern.DOTALL);
+
+    private static final MarkdownConverter markdownConverter = new MarkdownConverter();
 
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -25,11 +18,11 @@ public class Main {
         }
 
         try {
-            String markdownContent = readMarkdownFile(inputPath);
-            String htmlContent = convertMarkdownToHTML(markdownContent);
+            String markdownContent = markdownConverter.readMarkdownFile(inputPath);
+            String htmlContent = markdownConverter.convertMarkdownToHTML(markdownContent);
 
             if (outputPath != null) {
-                writeHTMLToFile(outputPath, "<p>" + htmlContent + "</p>");
+                markdownConverter.writeHTMLToFile(outputPath, "<p>" + htmlContent + "</p>");
                 System.out.println("HTML content saved to '" + outputPath + "'");
             } else {
                 System.out.println(htmlContent);
@@ -38,37 +31,6 @@ public class Main {
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
             System.exit(1);
-        }
-    }
-
-    private static String readMarkdownFile(String filePath) throws IOException {
-        StringBuilder content = new StringBuilder();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-        }
-
-        return content.toString();
-    }
-
-    private static String convertMarkdownToHTML(String markdownContent) {
-        markdownContent = boldRegEx.matcher(markdownContent).replaceAll("<b>$1</b>");
-
-        markdownContent = italicRegEx.matcher(markdownContent).replaceAll("<i>$1</i>");
-
-        markdownContent = preFormRegEx.matcher(markdownContent).replaceAll("<pre>$1</pre>");
-
-        markdownContent = monoRegEx.matcher(markdownContent).replaceAll("<tt>$1</tt>");
-
-        return markdownContent;
-    }
-
-    private static void writeHTMLToFile(String filePath, String htmlContent) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            writer.write(htmlContent);
         }
     }
 }
---
+++ lab1/src/MarkdownConverter.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.regex.Pattern;
+
+public class MarkdownConverter {
+    private final Pattern boldRegEx = Pattern.compile("\\*\\*(.*?)\\*\\*", Pattern.DOTALL);
+    private final Pattern italicRegEx = Pattern.compile("_(.*?)_", Pattern.DOTALL);
+    private final Pattern monoRegEx = Pattern.compile("`(.*?)`", Pattern.DOTALL);
+    private final Pattern preFormRegEx = Pattern.compile("```(.*?)```", Pattern.DOTALL);
+
+    public String readMarkdownFile(String filePath) throws IOException {
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+
+        return content.toString();
+    }
+
+    public String convertMarkdownToHTML(String markdownContent) {
+        markdownContent = boldRegEx.matcher(markdownContent).replaceAll("<b>$1</b>");
+
+        markdownContent = italicRegEx.matcher(markdownContent).replaceAll("<i>$1</i>");
+
+        markdownContent = preFormRegEx.matcher(markdownContent).replaceAll("<pre>$1</pre>");
+
+        markdownContent = monoRegEx.matcher(markdownContent).replaceAll("<tt>$1</tt>");
+
+        return markdownContent;
+    }
+
+    public void writeHTMLToFile(String filePath, String htmlContent) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(htmlContent);
+        }
+    }
+}
