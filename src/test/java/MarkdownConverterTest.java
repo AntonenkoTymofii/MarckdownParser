20a767009267a3cd619ce33e8eaf9b54e2a75f36
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-03-21 18:28:33 +0200

    edit path to TEST.md in tests

--- src/test/java/MarkdownConverterTest.java
+++ src/test/java/MarkdownConverterTest.java
@@ -1,5 +1,4 @@
 import org.example.MarkdownConverter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,19 +10,19 @@ class MarkdownConverterTest {
     @Test
     void readMarkdownFileTestHtml() throws IOException {
         Assertions.assertEquals("<p>\nHi, my name is **Tim**!\n</p>",
-                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\src\\test\\resources\\TEST.md", "html"));
+                markdownConverterTest.readMarkdownFile(".\\src\\test\\resources\\TEST.md", "html"));
     }
     @Test
     void readMarkdownFileTestNull() throws IOException {
         Assertions.assertEquals("<p>\nHi, my name is **Tim**!\n</p>",
-                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\src\\test\\resources\\TEST.md", null));
+                markdownConverterTest.readMarkdownFile(".\\src\\test\\resources\\TEST.md", null));
     }
     @Test
     void readMarkdownFileTestAnsi() throws IOException {
         Assertions.assertEquals("Hi, my name is **Tim**!\n",
-                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\src\\test\\resources\\TEST.md", "ansi"));
+                markdownConverterTest.readMarkdownFile(".\\src\\test\\resources\\TEST.md", "ansi"));
         Assertions.assertEquals("Hi, my name is **Tim**!\n",
-                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\src\\test\\resources\\TEST.md", "ANSI"));
+                markdownConverterTest.readMarkdownFile(".\\src\\test\\resources\\TEST.md", "ANSI"));
     }
 
     @Test
