1eb00302df2db8ada9b1db4cd92df4dfdd8ec12b
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-03-21 03:14:02 +0200

    create test class and some tests, also edit TEST.md for simple testing

---
+++ program/src/MarkdownConverterTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class MarkdownConverterTest {
+    MarkdownConverter markdownConverterTest = new MarkdownConverter();
+
+    @Test
+    void readMarkdownFileTestHtml() throws IOException {
+        Assertions.assertEquals("<p>\nHi, my name is **Tim**!\n</p>",
+                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\program\\src\\TEST.md", "html"));
+    }
+    @Test
+    void readMarkdownFileTestNull() throws IOException {
+        Assertions.assertEquals("<p>\nHi, my name is **Tim**!\n</p>",
+                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\program\\src\\TEST.md", null));
+    }
+    @Test
+    void readMarkdownFileTestAnsi() throws IOException {
+        Assertions.assertEquals("Hi, my name is **Tim**!\n",
+                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\program\\src\\TEST.md", "ansi"));
+        Assertions.assertEquals("Hi, my name is **Tim**!\n",
+                markdownConverterTest.readMarkdownFile("D:\\homework\\4 sem\\SDMT\\MarckdownParser\\program\\src\\TEST.md", "ANSI"));
+    }
+
+    @Test
+    void convertMarkdownToHTML() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void writeOutput() {
+    }
+}
--- program/src/TEST.md
+++ program/src/TEST.md
@@ -1,9 +1,1 @@
-Hi, my name _ is **Tim**!
-
-Hi, my name is _Tim_!
-
-Hi, my name is `Tim`!
-
-```
 Hi, my name is **Tim**!
-```
